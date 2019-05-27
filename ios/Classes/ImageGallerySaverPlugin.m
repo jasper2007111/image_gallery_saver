c9a18545cb95030ad58942cf28cd5a2a2bd2833b
jasper <jasper2007111@gmail.com>
2019-05-27 20:15:37 +0800

    移除swift相关文件

--- ios/Classes/ImageGallerySaverPlugin.m
+++ ios/Classes/ImageGallerySaverPlugin.m
@@ -1,8 +1,7 @@
 #import "ImageGallerySaverPlugin.h"
-#import <image_gallery_saver/image_gallery_saver-Swift.h>
 
 @implementation ImageGallerySaverPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftImageGallerySaverPlugin registerWithRegistrar:registrar];
+
 }
 @end
